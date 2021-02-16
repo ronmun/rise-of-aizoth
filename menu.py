8b3e5e90cef100d249b4201f9bb0d84fafda3a9c
ermiry <erick.salas@ermiry.com>
2021-02-16 11:22:22 -0600

    Added base main menu screen

---
+++ menu.py
@@ -0,0 +1,32 @@
+import pygame
+import os
+
+class MainMenu:
+	def __init__(self, w, h, win):
+		self.width = w
+		self.height = h
+		self.win = win
+		self.bg = None
+		# self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+
+	def run(self):
+		run = True
+
+		while run:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
+
+				if event.type == pygame.MOUSEBUTTONUP:
+					# check if hit start btn
+					x, y = pygame.mouse.get_pos()
+					# TODO: init game
+
+			self.draw()
+
+		pygame.quit()
+
+	def draw(self):
+		# TODO: draw sprites
+		# self.win.blit(self.bg, (0,0))
+		pygame.display.update()
