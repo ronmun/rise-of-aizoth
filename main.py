9bbf58dc2cd7b56fddf576feb9ddef9215c45e44
ermiry <erick.salas@ermiry.com>
2021-02-18 11:16:44 -0600

    Displaying background and title in menu screen

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ HEIGHT = 720
 if __name__ == "__main__":
 	pygame.init()
 	win = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption ('Rise of Aizoth')
 	mainMenu = MainMenu(WIDTH, HEIGHT, win)
 	mainMenu.run()
--- menu.py
+++ menu.py
@@ -1,13 +1,14 @@
-import pygame
 import os
+import pygame
 
 class MainMenu:
 	def __init__(self, w, h, win):
 		self.width = w
 		self.height = h
 		self.win = win
-		self.bg = None
-		# self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+		self.bg = pygame.image.load (os.path.join ("Assets/Sprites/Screens", "PantallaMenu.png"))
+		self.bg = pygame.transform.scale (self.bg, (self.width, self.height))
+		self.title = pygame.image.load (os.path.join ("Assets/Sprites/Screens", "GameTitle.png")).convert_alpha()
 
 	def run(self):
 		run = True
@@ -30,6 +31,6 @@ class MainMenu:
 		pygame.quit()
 
 	def draw(self):
-		# TODO: draw sprites
-		# self.win.blit(self.bg, (0,0))
+		self.win.blit(self.bg, (0,0))
+		self.win.blit(self.title, (self.width / 2 - self.title.get_width() / 2, 120))
 		pygame.display.update()
