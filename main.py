3becf5c00400a153597d403d84b0b16b27ec8c75
Aarón Gómez de Segura G <aaron.gomez.de.segura@gmail.com>
2021-02-19 14:19:08 -0600

    Changed window Icon

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
+import os
 
 from controller import Controller
 from state import State
 
 WIDTH = 960
 HEIGHT = 720
+ICON = pygame.image.load(os.path.join("Assets/Sprites", "Icon.png"))
 
 if __name__ == "__main__":
 	pygame.init()
 	win = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption ('Rise of Aizoth')
+	pygame.display.set_icon(ICON)
 
 	controller = Controller (WIDTH, HEIGHT, win)
 	controller.change (State.MENU, None)
