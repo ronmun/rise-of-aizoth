5a31c59ab38dc29d81100871f67e0e3b1b931e13
ermiry <erick.salas@ermiry.com>
2021-02-20 19:15:08 -0600

    Added base movement method in entity

--- Characters/entity.py
+++ Characters/entity.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 ENTITY_WIDTH = 16
@@ -11,7 +12,7 @@ class Entity:
 		self.animation_count = 0
 		self.health = 1
 		self.vel = 3
-		self.path = []
+		self.path = [(-50, 200), (780, 200), (780, 560), (180, 560), (180, 775)]
 		self.x = x
 		self.y = y
 		self.img = None
@@ -47,4 +48,42 @@ class Entity:
 		return False
 
 	def move (self):
-		pass
+		self.sprite_movement ()
+
+		x1, y1 = self.path[self.path_pos]
+		if self.path_pos + 1 >= len (self.path):
+			x2, y2 = (0,0 )
+		else:
+			x2, y2 = self.path[self.path_pos + 1]
+
+		dirn = ((x2-x1)*2, (y2-y1)*2)
+		length = math.sqrt((dirn[0])**2 + (dirn[1])**2)
+		dirn = (dirn[0]/length, dirn[1]/length)
+
+		if dirn[0] < 0 and not(self.flipped):
+			self.flipped = True
+			for x, img in enumerate(self.imgs):
+				self.imgs[x] = pygame.transform.flip(img, True, False)
+
+		move_x, move_y = ((self.x + dirn[0]), (self.y + dirn[1]))
+
+		self.x = move_x
+		self.y = move_y
+		print ("x " + str (self.x) + " - y " + str (self.y))
+
+		# Go to next point
+		if dirn[0] >= 0: # moving right
+			if dirn[1] >= 0: # moving down
+				if self.x >= x2 and self.y >= y2:
+					self.path_pos += 1
+			else:
+				if self.x >= x2 and self.y <= y2:
+					self.path_pos += 1
+
+		else: # moving left
+			if dirn[1] >= 0:  # moving down
+				if self.x <= x2 and self.y >= y2:
+					self.path_pos += 1
+			else:
+				if self.x <= x2 and self.y >= y2:
+					self.path_pos += 1
