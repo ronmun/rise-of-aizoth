9092e2686a9ca1c7d1749662eca3372593bfbca8
Aarón Gómez de Segura G <aaron.gomez.de.segura@gmail.com>
2021-02-22 13:50:08 -0600

    Fixed, end of level 3 bug, now returns to credits

--- Levels/esperanza.py
+++ Levels/esperanza.py
@@ -44,7 +44,7 @@ class Esperanza (Level):
 
                 if self.level_ui.won:
                     if self.level_ui.nextCheck(x, y):
-                        self.game.quit()
+                        self.game.change_to_credits()
                 if self.level_ui.lost:
                     if self.level_ui.retryCheck(x, y):
                         self.game.change(LevelState.ESPERANZA)
